7c0e74c6da5fa7ee02b7061e8c48182b1f6f7580
Cairoooo <101560245+Cairoooo@users.noreply.github.com>
2022-04-11 11:49:56 +0200

    Client finale (si spera)
    Viva la sorella di Pietro

---
+++ ClientAsta2/ClientAsta2/Asta.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace ClientAsta2
+{
+    //class Asta
+    //{
+    //    public string Nome { get; set; }
+    //    public Oggetto Oggetto { get; set; }
+    //    double baseAsta;
+    //    double incrementoMinimo;
+    //    int porta;
+    //    public double BaseAsta 
+    //    {
+    //        set
+    //        {
+    //            if(value > 0)
+    //            {
+    //                baseAsta = value;
+    //            }
+    //            else
+    //            {
+    //                BaseAsta = 1;
+    //            }
+    //        }
+    //        get { return baseAsta; }
+    //    }
+    //    public double IncrementoMinimo
+    //    {
+    //        set
+    //        {
+    //            if (value > 0)
+    //            {
+    //                IncrementoMinimo = value; 
+    //            } 
+    //            else
+    //            {
+    //                IncrementoMinimo=1; 
+    //            } 
+    //        } 
+    //        get { return IncrementoMinimo; }  
+
+    //    }
+    //    public int Porta
+    //    {
+    //        set
+    //        {
+    //            if (value > 0)
+    //            {
+    //                porta = value;
+    //            }
+    //            else
+    //            {
+    //                porta = 696969;
+    //            }
+    //        }
+    //        get { return porta; }
+    //    }
+    //    public Asta(string nome, Oggetto oggetto, double baseAsta, double incrementoMinimo, int porta)
+    //    {
+    //        Nome = nome;
+    //        Oggetto=oggetto;
+    //        BaseAsta=baseAsta; 
+    //        IncrementoMinimo=incrementoMinimo; 
+    //        Porta=porta;
+    //    }
+    //    public Asta(string xmlbyte)
+    //    {
+    //        string[] rawdata = xmlbyte.Split("<nome>", "</nome>", "<oggetto>", "</oggetto>", "<proprietario>", "</proprietario>", "<baseAsta>", "</baseAsta>", "<incrementoMinimo>", "</incrementoMinimo>", "<porta>", "</porta>");
+    //        Nome = rawdata[0];
+    //        Oggetto = new Oggetto(rawdata[1]);
+    //        BaseAsta = Convert.ToDouble(rawdata[2]);
+    //        incrementoMinimo = Convert.ToDouble(rawdata[3]);
+    //        porta = Convert.ToInt32(rawdata[4]);
+    //    }
+    //    public string GetXMLstring()
+    //    {
+    //        return string.Format("<asta>\n"+
+    //                             "\t<nome>{0}</nome>\n"+
+    //                             "\t<oggetto>{1}</oggetto>\n"+
+    //                             "\t<baseAsta>{2}</baseAsta>\n"+
+    //                             "\t<incrementoMinimo>{3}</incrementoMinimo>\n"+
+    //                             "\t<porta>{4}</porta>\n"+
+    //                             "</asta>\n", Nome, Oggetto.GetXMLstring(), Proprietario.GetXMLstring(), baseAsta, incrementoMinimo, porta);
+    //    }
+    //}
+}
---
+++ ClientAsta2/ClientAsta2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientAsta2
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_conferma_Click(object sender, RoutedEventArgs e)
+        {
+            Client c;
+            double budget;
+            if (!txt_budget.Text.Equals(string.Empty) && double.TryParse(txt_budget.Text, out budget) && double.Parse(txt_budget.Text) > 0)
+            {
+                c = new Client(double.Parse(txt_budget.Text));
+                c.Attivita();
+                Lbl_Errore.Content = string.Empty;
+                Main.Content = new PaginaSceltaAsta(c.Budget, c.Stream);
+                Scompari.Children.Clear();
+            }
+            else
+            {
+                Lbl_Errore.Content = "Valore Impossibile!!!";
+            }
+        }
+    }
+}
